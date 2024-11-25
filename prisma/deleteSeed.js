45929e01ab9a5c014d2435efda5f4057196ff78b
krisnaepras <krisnaepras@gmail.com>
2024-11-25 12:20:57 +0700

    add reset autoincrement to delete seed

--- prisma/deleteSeed.js
+++ prisma/deleteSeed.js
@@ -9,28 +9,33 @@ async function clearDatabase() {
     // 1. Hapus Seats
     console.log("Deleting seats...");
     await prisma.seat.deleteMany();
-
+    await prisma.$executeRaw`ALTER TABLE seat AUTO_INCREMENT = 1`; 
     // 2. Hapus Flights
     console.log("Deleting flights...");
     await prisma.flight.deleteMany();
+    await prisma.$executeRaw`ALTER TABLE flight AUTO_INCREMENT = 1`; 
 
     // 3. Hapus Airlines
     console.log("Deleting airlines...");
     await prisma.airline.deleteMany();
+    await prisma.$executeRaw`ALTER TABLE airline AUTO_INCREMENT = 1`;
 
     // 4. Hapus Airports
     console.log("Deleting airports...");
     await prisma.airport.deleteMany();
+    await prisma.$executeRaw`ALTER TABLE airport AUTO_INCREMENT = 1`;
 
     // 5. Hapus Passengers
     console.log("Deleting passengers...");
     await prisma.passanger.deleteMany();
+    await prisma.$executeRaw`ALTER TABLE passanger AUTO_INCREMENT = 1`; 
 
     // 6. Hapus Users
     console.log("Deleting users...");
     await prisma.user.deleteMany();
+    await prisma.$executeRaw`ALTER TABLE user AUTO_INCREMENT = 1`; 
 
-    console.log("Database cleared successfully!");
+    console.log("Database cleared and IDs reset successfully!");
   } catch (error) {
     console.error("Error during clearing database:", error);
     throw error;
