236b19c7ac364651dc0b1dc4875d6e210ff15028
0xtbug <tubagus.nabil19@gmail.com>
2024-12-03 20:21:18 +0700

    perf: login routes

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,7 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const LoginController = require("../controllers/LoginController");
+const AuthMiddleware = require('../middleware/authMiddleware');
+const LoginController = require('../controllers/loginController');
 
-router.post("/", LoginController.login);
+// Login routes
+router.post('/login', LoginController.login);
+router.post('/refresh-token', LoginController.refreshToken);
+router.post('/logout', AuthMiddleware.verifyToken, LoginController.logout);
+
+// Profile route
+// router.get('/profile', AuthMiddleware.verifyToken, (req, res) => {
+//     const user = {
+//         id: req.user.id,
+//         name: req.user.name,
+//         email: req.user.email,
+//         role: req.user.role,
+//         phoneNumber: req.user.phoneNumber,
+//         is_verified: req.user.is_verified
+//     };
+//     res.json({ 
+//         status: "success", 
+//         data: user 
+//     });
+// });
 
 module.exports = router;
