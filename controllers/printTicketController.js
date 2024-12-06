c78a94eb9a91072e483f142b4052f405496cee15
krisnaepras <krisnaepras@gmail.com>
2024-12-06 21:57:00 +0700

    add print ticket

---
+++ controllers/printTicketController.js
@@ -0,0 +1,6 @@
+const prisma = require("../models/prismaClients");
+const response = require("../utils/response");
+
+class PrintTicketController {
+    // buatkan controller untuk cetak pdf isinya semua tiket yang telah dibuat dalam 1 transaksi dan dikirim ke respon dalam bentuk qrcode yang isinya link download pdf    
+}
