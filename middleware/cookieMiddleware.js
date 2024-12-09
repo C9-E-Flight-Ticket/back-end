8e57f72f3650053b6baf080203722615d43310c5
krisnaepras <161457644+krisnaepras@users.noreply.github.com>
2024-12-09 22:02:54 +0700

    Update cookieMiddleware.js sementara

--- middleware/cookieMiddleware.js
+++ middleware/cookieMiddleware.js
@@ -3,7 +3,7 @@ class CookieMiddleware {
     static setTokenCookie(res, token) {
         res.cookie('access_token', token, {
             httpOnly: false,
-            secure: process.env.NODE_ENV === 'production',
+            secure: false, // process.env.NODE_ENV === 'production',
             sameSite: 'none',
             maxAge: process.env.COOKIE_EXPIRED * 60 * 60 * 1000
         });
@@ -12,7 +12,7 @@ class CookieMiddleware {
     static clearTokenCookie(res) {
         const cookieOptions = {
             httpOnly: false,
-            secure: process.env.NODE_ENV === 'production',
+            secure: false, // process.env.NODE_ENV === 'production',
             sameSite: 'none',
             maxAge: 0
         };
