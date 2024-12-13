7cb16fb63fcef5b355ed74d42b1bb61feb046b64
okkysatria <okkysatria11@gmail.com>
2024-12-13 16:45:58 +0700

    add passenger

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const flightRoute = require('./routes/flightRoutes')
 const seatRoute = require('./routes/seatRoutes')
 const notificationRoutes = require('./routes/notificationRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const passengerRoute = require('./routes/passengerRoutes');
 
 const corsOptions = {
   origin: [
@@ -63,6 +64,7 @@ app.use('/api/transaction', transactionRoutes);
 app.use('/api/flight', flightRoute)
 app.use('/api/seat', seatRoute)
 app.use('/api/notifications', notificationRoutes)
+app.use('/api/passenger', passengerRoute);
 
 // Request logging middleware
 app.use((req, res, next) => {
