0f8ff9ac6d36b9ca62c63b27ad2a6e1ef5c0113e
krisnaepras <krisnaepras@gmail.com>
2024-12-20 19:39:53 +0700

    delete middleware for download ticket

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -5,6 +5,8 @@ const AuthMiddleware = require('../middleware/authMiddleware');
 
 const TransactionController = require("../controllers/transactionController");
 
+router.get('/download/:bookingCode.pdf', asyncErrorHandler(TransactionController.downloadPDF));
+
 router.use(AuthMiddleware.verifyAuthentication);
 
 router.post("/order", asyncErrorHandler(TransactionController.createTicketTransaction));
@@ -12,6 +14,5 @@ router.post("/midtrans-callback", asyncErrorHandler(TransactionController.handle
 router.get('/status/:bookingCode', asyncErrorHandler(TransactionController.getTransactionStatus));
 router.get('/transactions', asyncErrorHandler(TransactionController.getAllTransactionsByUser));
 router.get('/generate-pdf/:bookingCode', asyncErrorHandler(TransactionController.generateTransactionPDF));
-router.get('/download/:bookingCode.pdf', asyncErrorHandler(TransactionController.downloadPDF));
 
 module.exports = router;
